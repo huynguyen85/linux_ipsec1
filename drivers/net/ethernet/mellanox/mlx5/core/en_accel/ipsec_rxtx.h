5ecbb1d61484bfd63598ad24cb6a90ac2203e146
Raed Salem <raeds@mellanox.com>
2020-04-08 20:55:30 +0300

    net/mlx5e: IPsec: Add Connect-X IPsec Tx data path offload
    In the TX data path, spot packets with xfrm stack IPsec offload
    indication.
    
    Fill Software-Parser segment in TX descriptor so that the hardware
    may parse the ESP protocol, and perform TX checksum offload on the
    inner payload.
    
    Support GSO, by providing the trailer data and ICV placeholder
    so HW can fill it post encryption operation.
    
    Padding alignment cannot be performed in HW (ConnectX-6Dx) due to
    a bug.
    Software can overcome this limitation by adding NETIF_F_HW_ESP to
    the gso_partial_features field in netdev so the packets being
    aligned by the stack.
    
    Note that for GSO SKBs, the stack does not include an ESP trailer,
    unlike the non-GSO case.
    
    issue: 1890739
    Change-Id: I0adfab6df6828c39b336e4c5c3bad60166275bac
    Signed-off-by: Raed Salem <raeds@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_accel/ipsec_rxtx.h
+++ drivers/net/ethernet/mellanox/mlx5/core/en_accel/ipsec_rxtx.h
@@ -56,9 +56,10 @@ void mlx5e_ipsec_set_iv_esn(struct sk_buff *skb, struct xfrm_state *x,
 			    struct xfrm_offload *xo);
 void mlx5e_ipsec_set_iv(struct sk_buff *skb, struct xfrm_state *x,
 			struct xfrm_offload *xo);
-bool mlx5e_ipsec_handle_tx_skb(struct mlx5e_priv *priv,
-			       struct mlx5_wqe_eth_seg *eseg,
-			       struct sk_buff *skb);
+struct sk_buff *mlx5e_ipsec_handle_tx_skb(struct net_device *netdev,
+					  struct mlx5e_txqsq *sq,
+					  struct mlx5e_tx_wqe *wqe,
+					  struct sk_buff *skb);
 void mlx5e_ipsec_offload_handle_rx_skb(struct net_device *netdev,
 				       struct sk_buff *skb,
 				       struct mlx5_cqe64 *cqe);

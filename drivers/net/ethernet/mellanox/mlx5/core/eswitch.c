ebd228d6621a5e7b14303f3e010a52cce328279a
Saeed Mahameed <saeedm@nvidia.com>
2020-12-03 11:55:16 +0200

    net/mlx5: Don't skip vport check
    Users of mlx5_eswitch_get_vport() are required to check return value
    prior to passing mlx5_vport further. Fix all the places to do not skip
    that check.
    
    Fixes: c7eddc6092b4 ("net/mlx5: E-switch, Move devlink eswitch ports closer to eswitch")
    Fixes: 5d9986a3947a ("net/mlx5: E-Switch, Fix the check of legal vport")
    Reviewed-by: Eli Cohen <elic@nvidia.com>
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
    
    # Conflicts:
    #	drivers/net/ethernet/mellanox/mlx5/core/eswitch.c

--- drivers/net/ethernet/mellanox/mlx5/core/eswitch.c
+++ drivers/net/ethernet/mellanox/mlx5/core/eswitch.c
@@ -1142,6 +1142,8 @@ int mlx5_esw_modify_vport_rate(struct mlx5_eswitch *esw, u16 vport_num,
 	struct mlx5_vport *vport;
 
 	vport = mlx5_eswitch_get_vport(esw, vport_num);
+	if (IS_ERR(vport))
+		return PTR_ERR(vport);
 
 	if (!vport->qos.enabled)
 		return -EOPNOTSUPP;
@@ -1280,6 +1282,8 @@ int mlx5_esw_vport_enable(struct mlx5_eswitch *esw, u16 vport_num,
 	int ret;
 
 	vport = mlx5_eswitch_get_vport(esw, vport_num);
+	if (IS_ERR(vport))
+		return PTR_ERR(vport);
 
 	mutex_lock(&esw->state_lock);
 	WARN_ON(vport->enabled);
@@ -1315,6 +1319,8 @@ void mlx5_esw_vport_disable(struct mlx5_eswitch *esw, u16 vport_num)
 	struct mlx5_vport *vport;
 
 	vport = mlx5_eswitch_get_vport(esw, vport_num);
+	if (IS_ERR(vport))
+		return;
 
 	mutex_lock(&esw->state_lock);
 	if (!vport->enabled)
